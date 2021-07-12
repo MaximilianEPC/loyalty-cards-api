66354608c9e78f86b2b1b0f07a082c73ac267f5f
@maksim.ivanovskij <maximilianepc@outlook.com>
2021-07-12 16:53:54 +0300

    Small cleanup.

--- LoyaltyCardsAPI/Controllers/TransactionController.cs
+++ LoyaltyCardsAPI/Controllers/TransactionController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-using System.Xml;
 using System.Xml.Serialization;
 
 namespace LoyaltyCardsAPI.Controllers
